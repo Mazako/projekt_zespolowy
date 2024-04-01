421dcb9e164f70dea2aba2e38c65031f278e07b0
michał <michal.maziarz12@gmail.com>
2024-04-01 23:22:54 +0200

    Pan-Tompkins algorithm

---
+++ ecd_sandbox/EcdDataConverter.py
@@ -0,0 +1,4 @@
+def import_mat(hea_file_path, mat_file_path):
+    #TODO: funkcja musi zczytać z patha nagłówek, wyciągnąć z niego ważne dane
+    # Zczytać plik .mat, wczytać dane, znormalizować je i zwrócić obiekt EcdData
+    pass
