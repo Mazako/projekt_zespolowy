64f0f0b0c3d618e9977513b66a07a7b4d83ad02d
michał <michal.maziarz12@gmail.com>
2024-04-21 18:39:02 +0200

    drag n drop

--- frontend/components/send_ecd/FileContainer.tsx
+++ frontend/components/send_ecd/FileContainer.tsx
@@ -1,12 +1,14 @@
-import React, {FC} from "react";
+import React, {ChangeEvent, DragEventHandler, FC} from "react";
 import styles from './FileContainer.module.css'
 
-export const FileContainer: FC<{
+type FileContainerProps = {
     title: string,
     file: File | null,
     setFile: React.Dispatch<React.SetStateAction<File | null>>
-}> = ({title, file, setFile}) => {
+}
+export const FileContainer: FC<FileContainerProps> = ({title, file, setFile}) => {
     const fileRef = React.useRef<HTMLInputElement>(null);
+    const [drag, setDrag] = React.useState(false);
 
     const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (e.target.files) {
@@ -20,8 +22,30 @@ export const FileContainer: FC<{
         }
     }
 
+    const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        e.stopPropagation();
+        setDrag(true);
+    };
+
+    const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        e.stopPropagation();
+        setDrag(false);
+    }
+
+    const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        e.stopPropagation();
+        if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
+            setFile(e.dataTransfer.files[0]);
+        }
+        setDrag(false);
+    };
+
+
     return (<>
-        <div className={styles.fc} onClick={handleButtonClick}>
+        <div className={drag ? styles.hover : styles.fc} onClick={handleButtonClick} onDrop={handleDrop} onDragOver={handleDragOver} onDragLeave={handleDragLeave}>
             <img src='/icons/file.svg' alt="file icon" width={50} height={50}/>
             <p className='text-primary'>{title}</p>
             {file == null || <p className={styles.spacer}>Wybrano plik: {file.name}</p>}
